fe308310ecc1bcc8fb1ddf2c51a380b1be863f9b
TeaSetso <89183763+TeaSetso@users.noreply.github.com>
2021-08-20 17:47:44 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG522_FA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How would you describe yourself in three words: ");
+            Console.ReadLine();
+            Console.WriteLine("Would you describe yourself as an approachable person : ");
+            Console.ReadLine();
+            Console.WriteLine("Thank You For Completing The Survey");
+            Console.ReadLine();
+        }
+    }
+}
