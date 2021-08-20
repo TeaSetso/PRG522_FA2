2dbeeb424b9a1f8c6d04a84f3171490abfe7f585
TeaSetso <89183763+TeaSetso@users.noreply.github.com>
2021-08-20 18:12:43 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,9 +15,7 @@ namespace PRG522_FA2
             Console.WriteLine("Would you describe yourself as an approachable person : ");
             Console.ReadLine();
             Console.WriteLine("Thank You For Completing The Survey !");
-            Console.ReadLine();
-            Console.WriteLine("Would you like to view another survey ?");
-
+          
 
         }
     }
