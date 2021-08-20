8c155852230ad75123a5924b8dd94d057488b4c0
TeaSetso <89183763+TeaSetso@users.noreply.github.com>
2021-08-20 18:05:49 +0200

    Minor Changes

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace PRG522_FA2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How would you describe yourself in three words: ");
+            Console.WriteLine("How would you describe yourself in three words : ");
             Console.ReadLine();
             Console.WriteLine("Would you describe yourself as an approachable person : ");
             Console.ReadLine();
-            Console.WriteLine("Thank You For Completing The Survey");
+            Console.WriteLine("Thank You For Completing The Survey !");
             Console.ReadLine();
+            Console.WriteLine("Would you like to view another survey ?");
+
+
         }
     }
 }
